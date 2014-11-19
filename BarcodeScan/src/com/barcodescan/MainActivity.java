2e87339cd303a9995848f11632135890c4e326a7
David Germishuys <dgermishuys9@gmail.com>
2014-11-19 16:03:38 +0200

    Scan Button Added

--- BarcodeScan/src/com/barcodescan/MainActivity.java
+++ BarcodeScan/src/com/barcodescan/MainActivity.java
@@ -11,8 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+
 public class MainActivity extends ActionBarActivity {
 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,9 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
+       
+        
     }
 
 
