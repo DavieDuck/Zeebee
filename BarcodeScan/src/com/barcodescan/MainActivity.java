1de3e328b6b69a9d6617ee605e08f5ab8945af3e
David Germishuys <dgermishuys9@gmail.com>
2014-12-02 07:38:40 +0200

    Zbar Functionality, Text Feedback

--- BarcodeScan/src/com/barcodescan/MainActivity.java
+++ BarcodeScan/src/com/barcodescan/MainActivity.java
@@ -1,71 +1,152 @@
 package com.barcodescan;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import net.sourceforge.zbar.Config;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.hardware.Camera;
+import android.hardware.Camera.AutoFocusCallback;
+import android.hardware.Camera.PreviewCallback;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+//import com.barcodescan.BarcodeData;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
+	private Camera mCamera;
+	private CameraPreview mPreview;
+	private Handler autoFocusHandler;
 
+	TextView scanText;
+	Button scanButton;
+
+	ImageScanner scanner;
+
+	private boolean barcodeScanned = false;
+	private boolean previewing = true;
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	Animation animFadein;
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+	static {
+		System.loadLibrary("iconv");
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		
+		// load the animation
+        animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_in);  
+
+		autoFocusHandler = new Handler();
+		mCamera = getCameraInstance();
+
+		// Instance barcode scanner
+		scanner = new ImageScanner();
+		scanner.setConfig(0, Config.X_DENSITY, 3);
+		scanner.setConfig(0, Config.Y_DENSITY, 3);
+
+		mPreview = new CameraPreview(this, mCamera, previewCb, autoFocusCB);
+		FrameLayout preview = (FrameLayout) findViewById(R.id.cameraPreview);
+		preview.addView(mPreview);
+
+		scanText = (TextView) findViewById(R.id.scanText);
+
+		scanButton = (Button) findViewById(R.id.button1);
+
+		scanButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (barcodeScanned) {
+					barcodeScanned = false;
+					scanText.setText("Scanning...");
+					mCamera.setPreviewCallback(previewCb);
+					mCamera.startPreview();
+					previewing = true;
+					mCamera.autoFocus(autoFocusCB);
+				}
+			}
+		});
+
+	}
+
+	public void onPause() {
+		super.onPause();
+		releaseCamera();
+	}
+
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open();
+		} catch (Exception e) {
+		}
+		return c;
+	}
+
+	private void releaseCamera() {
+		if (mCamera != null) {
+			previewing = false;
+			mCamera.setPreviewCallback(null);
+			mCamera.release();
+			mCamera = null;
+		}
+	}
+	
+	private Runnable doAutoFocus = new Runnable() {
+        public void run() {
+            if (previewing)
+                mCamera.autoFocus(autoFocusCB);
         }
-        
-       
-        
-    }
+    };
+    
+    PreviewCallback previewCb = new PreviewCallback() {
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            Camera.Parameters parameters = camera.getParameters();
+            Size size = parameters.getPreviewSize();
 
+            Image barcode = new Image(size.width, size.height, "Y800");
+            barcode.setData(data);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+            int result = scanner.scanImage(barcode);
+            
+            if (result != 0) {
+                previewing = false;
+                mCamera.setPreviewCallback(null);
+                mCamera.stopPreview();
+                
+                
+                //for (int i =0; i < )
+                
+                SymbolSet syms = scanner.getResults();
+                for (Symbol sym : syms) {
+                    scanText.setText("barcode result " + sym.getData());
+                    barcodeScanned = true;
+                }
+            }
         }
-        return super.onOptionsItemSelected(item);
-    }
+    };
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
+// Mimic continuous auto-focusing
+AutoFocusCallback autoFocusCB = new AutoFocusCallback() {
+        public void onAutoFocus(boolean success, Camera camera) {
+            autoFocusHandler.postDelayed(doAutoFocus, 1000);
         }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+    };
 
 }
